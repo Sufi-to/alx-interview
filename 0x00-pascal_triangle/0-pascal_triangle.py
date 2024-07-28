0a8c77b94148f48f3b874612b50aca8a5c726583
Sufi-to <3thomas225@gmail.com>
2024-07-28 17:33:21 +0400

    print pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module for pascal_triangle"""
+
+def pascal_triangle(n):
+    """Returns pascal's triangel."""
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
