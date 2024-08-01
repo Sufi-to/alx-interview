34d2d640cfe6b15e8362eaad13bb745434ba8d2c
Sufi-to <3thomas225@gmail.com>
2024-08-01 22:22:03 +0400

    optimzed lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,20 +4,17 @@
 
 def canUnlockAll(boxes):
     """Returns a boolean of whether all the lockboxes can be unlocked."""
-    keyLocks = set()
-    for i in range(1, len(boxes)):
-        if i in boxes[i-1]:
-            for j in boxes[i-1]:
-                keyLocks.add(j)
-            for x in boxes[i]:
-                keyLocks.add(x)
-    for y in range(len(boxes)):
-        for i in boxes:
-            if boxes.index(i) in keyLocks:
-                for j in i:
-                    keyLocks.add(j)
-    setKeys = set(keyLocks)
-    for i in range(1, len(boxes)-1):
-        if i not in setKeys:
+    keyLocks = set([0])
+    keys_to_check = [0]
+
+    while keys_to_check:
+        current_key = keys_to_check.pop()
+        for key in boxes[current_key]:
+            if key not in keyLocks:
+                keyLocks.add(key)
+                if key < len(boxes):
+                    keys_to_check.append(key)
+    for i in range(len(boxes)):
+        if i not in keyLocks:
             return False
     return True
