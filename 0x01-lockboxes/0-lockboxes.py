338a19857f133d7a5cd59197075ade25329df9de
Sufi-to <3thomas225@gmail.com>
2024-08-01 22:00:42 +0400

    using set to use less memory

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,18 +4,18 @@
 
 def canUnlockAll(boxes):
     """Returns a boolean of whether all the lockboxes can be unlocked."""
-    keyLocks = []
+    keyLocks = set()
     for i in range(1, len(boxes)):
         if i in boxes[i-1]:
             for j in boxes[i-1]:
-                keyLocks.append(j)
+                keyLocks.add(j)
             for x in boxes[i]:
-                keyLocks.append(x)
+                keyLocks.add(x)
     for y in range(len(boxes)):
         for i in boxes:
             if boxes.index(i) in keyLocks:
                 for j in i:
-                    keyLocks.append(j)
+                    keyLocks.add(j)
     setKeys = set(keyLocks)
     for i in range(1, len(boxes)-1):
         if i not in setKeys:
