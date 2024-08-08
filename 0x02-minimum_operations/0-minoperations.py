4f59f2e5367957fa64eafb5a38581886a6ceadc1
Sufi-to <3thomas225@gmail.com>
2024-08-09 03:56:13 +0400

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that shows how to find the minimum operations needed to perform the task."""
+
+
+def minOperations(n):
+    """Returns the factorial of n which is the min operations needed."""
+    num = 2
+    res = 0
+    while n > 1:
+        while n % num == 0:
+            res += num
+            n /= num
+        num += 1
+    return res
