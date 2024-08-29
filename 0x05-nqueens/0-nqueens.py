c0d78de0cf423eae2d537e8f588430b2930b7220
Sufi-to <3thomas225@gmail.com>
2024-08-30 03:45:04 +0400

    typo fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,15 +5,15 @@ from sys import argv
 
 
 if len(argv) > 2 or len(argv) < 2:
-    print("Usage: nqueens n_queen")
+    print("Usage: nqueens N")
     exit(1)
 
 if not argv[1].isdigit():
-    print("n_queen must be columns number")
+    print("N must be columns number")
     exit(1)
 
 if int(argv[1]) < 4:
-    print("n_queen must be at least 4")
+    print("N must be at least 4")
     exit(1)
 
 n_queen = int(argv[1])
