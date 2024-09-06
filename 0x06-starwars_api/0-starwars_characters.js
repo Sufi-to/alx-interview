eaca715945d18467ef4e46288c5fd68a0228c397
Sufi-to <3thomas225@gmail.com>
2024-09-06 06:53:06 +0400

    fetch data

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,28 @@
 #!/usr/bin/node
-const argv = process.argv;
-const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
-const urlMovie = `${urlFilm}${argv[2]}/`;
-
 const axios = require('axios');
-
+const argv = process.argv;
+const movieId = argv[2];
+const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
+const urlMovie = `${urlFilm}${movieId}/`;
 
 axios.get(urlMovie)
   .then(response => {
     const characters = response.data.characters;
-
     if (characters && characters.length > 0) {
-      const limit = characters.length;
-      CharRequest(0, characters, limit);
+      fetchCharacters(characters);
     }
   })
   .catch(error => {
-    console.error('Error fetching movie:', error);
+    console.error('Error fetching movie:', error.message);
   });
 
-function CharRequest (idx, characters, limit) {
-  if (idx === limit) {
-    return;
-  }
-
-  axios.get(characters[idx])
-    .then(response => {
+async function fetchCharacters(characters) {
+  for (const characterUrl of characters) {
+    try {
+      const response = await axios.get(characterUrl);
       console.log(response.data.name);
-      idx++;
-      CharRequest(idx, characters, limit);
-    })
-    .catch(error => {
-      console.error('Error fetching character:', error);
-    });
+    } catch (error) {
+      console.error('Error fetching character:', error.message);
+    }
+  }
 }
